c3044095899467ad6831d1f19338d589c1b9a7fa
thander <thander_pep@hotmail.com>
2022-08-17 02:16:28 -0300

    Criando estrutura basica do projeto

---
+++ src/test/java/br/com/thander/sonda/SondaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thander.sonda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
