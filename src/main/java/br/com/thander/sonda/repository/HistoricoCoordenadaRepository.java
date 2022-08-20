de85e7fbb7856806f1be20b6f5538b86fbba378d
thander <thander_pep@hotmail.com>
2022-08-20 04:06:29 -0300

    Mudanças na lógica das tabelas

--- src/main/java/br/com/thander/sonda/repository/HistoricoCoordenadaRepository.java
+++ src/main/java/br/com/thander/sonda/repository/HistoricoCoordenadaRepository.java
@@ -3,5 +3,5 @@ package br.com.thander.sonda.repository;
 import br.com.thander.sonda.model.entity.HistoricoCoordenadaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CoordenadaRepository extends JpaRepository<HistoricoCoordenadaEntity, Long> {
+public interface HistoricoCoordenadaRepository extends JpaRepository<HistoricoCoordenadaEntity, Long> {
 }
